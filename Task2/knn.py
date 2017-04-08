4db7f8d5fe22bc734b7846d56bc71cc910bbb478
MinaMedic <alekmina94@gmail.com>
2017-04-08 16:47:50 +0200

    Izbacena obelezja

--- Task2/knn.py
+++ Task2/knn.py
@@ -16,9 +16,77 @@ from sklearn.metrics.pairwise import euclidean_distances
     trainSetGrades, testSetGrades - sets with only data from 'Grade' column
     trainSetFeatures, testSetFeatures - sets with all attributes except 'Grade'
 '''
+def load_dataset(trainFileName, testFileName):
+    dataTrain = pd.read_csv(trainFileName, header=0)
+    
+    #list with only Y values from dataset
+    trainSetGrades = dataTrain['Grade'].values.tolist()
+    
+    #transform to numerical
+    dataTrain['sex'] = dataTrain['sex'].map({'F': 0, 'M': 1})
+    dataTrain['address'] = dataTrain['address'].map({'U': 0, 'R': 1})
+    dataTrain['famsize'] = dataTrain['famsize'].map({'LE3': 0, 'GT3': 1})
+    dataTrain['Pstatus'] = dataTrain['Pstatus'].map({'T': 0, 'A': 1})
+    dataTrain['reason'] = dataTrain['reason'].map({'home': 0, 'reputation': 1, 'course' : 2, 'other' : 3})
+    dataTrain['guardian'] = dataTrain['guardian'].map({'mother': 0, 'father': 1, 'other' : 2})
+    dataTrain['schoolsup'] = dataTrain['schoolsup'].map({'yes': 0, 'no': 1})
+    dataTrain['famsup'] = dataTrain['famsup'].map({'yes': 0, 'no': 1})
+    dataTrain['paid'] = dataTrain['paid'].map({'yes': 0, 'no': 1})
+    dataTrain['activities'] = dataTrain['activities'].map({'yes': 0, 'no': 1})
+    dataTrain['higher'] = dataTrain['higher'].map({'yes': 0, 'no': 1})
+    dataTrain['internet'] = dataTrain['internet'].map({'yes': 0, 'no': 1})
+    dataTrain['romantic'] = dataTrain['romantic'].map({'yes': 0, 'no': 1})
+    
+    #transform from int to float
+    dataTrain  = dataTrain.astype(float)
+    
+    #drop unnecessary attributess from dataset
+    dataTrain = dataTrain.drop(['age', 'Fedu', 'reason', 'traveltime', 'famsup', 'famrel', 'freetime', 'goout', 'Walc', 'Medu', 'health', 'absences', 'guardian'], axis = 1)
+    trainingSet = dataTrain.values.tolist()
+    
+    #list with only X attributes from train dataset
+    trainSetFeatures = dataTrain.drop('Grade', axis=1).values.tolist()
+        
+    
+    
+    dataTest = pd.read_csv(testFileName, header=0)
+    
+    #list with only Y values from dataset
+    testSetGrades = dataTest['Grade'].values.tolist()
+    
+    #transform to numerical
+    dataTest['sex'] = dataTest['sex'].map({'F': 0, 'M': 1})
+    dataTest['address'] = dataTest['address'].map({'U': 0, 'R': 1})
+    dataTest['famsize'] = dataTest['famsize'].map({'LE3': 0, 'GT3': 1})
+    dataTest['Pstatus'] = dataTest['Pstatus'].map({'T': 0, 'A': 1})
+    dataTest['reason'] = dataTest['reason'].map({'home': 0, 'reputation': 1, 'course' : 2, 'other' : 3})
+    dataTest['guardian'] = dataTest['guardian'].map({'mother': 0, 'father': 1, 'other' : 2})
+    dataTest['schoolsup'] = dataTest['schoolsup'].map({'yes': 0, 'no': 1})
+    dataTest['famsup'] = dataTest['famsup'].map({'yes': 0, 'no': 1})
+    dataTest['paid'] = dataTest['paid'].map({'yes': 0, 'no': 1})
+    dataTest['activities'] = dataTest['activities'].map({'yes': 0, 'no': 1})
+    dataTest['higher'] = dataTest['higher'].map({'yes': 0, 'no': 1})
+    dataTest['internet'] = dataTest['internet'].map({'yes': 0, 'no': 1})
+    dataTest['romantic'] = dataTest['romantic'].map({'yes': 0, 'no': 1})
+    
+    #transform from int to float
+    dataTest  = dataTest.astype(float)
+    
+    #drop unnecessary attributess from dataset
+    dataTest = dataTest.drop(['age', 'Fedu', 'reason', 'traveltime', 'famsup', 'famrel', 'freetime', 'goout', 'Walc', 'Medu', 'health', 'absences', 'guardian'], axis = 1)
+    testSet = dataTest.values.tolist()
+    
+    #list with only X attributes from test dataset
+    testSetFeatures = dataTest.drop('Grade', axis=1).values.tolist()
+    
+    
+    return trainingSet, testSet, trainSetGrades, testSetGrades, trainSetFeatures, testSetFeatures
+
+  
+    
 #def load_dataset(trainFileName, testFileName):
 #    dataTrain = pd.read_csv(trainFileName, header=0)
-#    trainSetGrades = dataTrain['Grade'].values.tolist()
+#    trainSetGrades = dataTrain['Grade'].values.tolist()[:400]
 #    
 #    dataTrain['sex'] = dataTrain['sex'].map({'F': 0, 'M': 1})
 #    dataTrain['address'] = dataTrain['address'].map({'U': 0, 'R': 1})
@@ -35,91 +103,34 @@ from sklearn.metrics.pairwise import euclidean_distances
 #    dataTrain['romantic'] = dataTrain['romantic'].map({'yes': 0, 'no': 1})
 #    
 #    dataTrain  = dataTrain.astype(float)
-#    trainingSet = dataTrain.values.tolist()
-#    trainSetFeatures = dataTrain.drop('Grade', axis=1).values.tolist()
+#    dataTrain = dataTrain.drop(['age', 'Fedu', 'reason', 'traveltime', 'famsup', 'famrel', 'freetime', 'goout', 'Walc', 'Medu', 'health', 'absences', 'guardian'], axis = 1)
+#    
+#    dataTrainCopy = deepcopy(dataTrain)
+#    
+#    trainingSet = dataTrain.values.tolist()[:400]
+#    trainSetFeatures = dataTrain.drop('Grade', axis=1).values.tolist()[:400]
 #        
 #    
 #    
-#    dataTest = pd.read_csv(testFileName, header=0)
-#    testSetGrades = dataTest['Grade'].values.tolist()
+#    #dataTest = pd.read_csv(testFileName, header=0)
+#    testSetGrades = dataTrainCopy['Grade'].values.tolist()[400:]
 #    
-#    dataTest['sex'] = dataTest['sex'].map({'F': 0, 'M': 1})
-#    dataTest['address'] = dataTest['address'].map({'U': 0, 'R': 1})
-#    dataTest['famsize'] = dataTest['famsize'].map({'LE3': 0, 'GT3': 1})
-#    dataTest['Pstatus'] = dataTest['Pstatus'].map({'T': 0, 'A': 1})
-#    dataTest['reason'] = dataTest['reason'].map({'home': 0, 'reputation': 1, 'course' : 2, 'other' : 3})
-#    dataTest['guardian'] = dataTest['guardian'].map({'mother': 0, 'father': 1, 'other' : 2})
-#    dataTest['schoolsup'] = dataTest['schoolsup'].map({'yes': 0, 'no': 1})
-#    dataTest['famsup'] = dataTest['famsup'].map({'yes': 0, 'no': 1})
-#    dataTest['paid'] = dataTest['paid'].map({'yes': 0, 'no': 1})
-#    dataTest['activities'] = dataTest['activities'].map({'yes': 0, 'no': 1})
-#    dataTest['higher'] = dataTest['higher'].map({'yes': 0, 'no': 1})
-#    dataTest['internet'] = dataTest['internet'].map({'yes': 0, 'no': 1})
-#    dataTest['romantic'] = dataTest['romantic'].map({'yes': 0, 'no': 1})
-#    
-#    dataTest  = dataTest.astype(float)
-#    testSet = dataTest.values.tolist()
-#    testSetFeatures = dataTest.drop('Grade', axis=1).values.tolist()
+#    testSet = dataTrainCopy.values.tolist()[400:]
+#    testSetFeatures = dataTrainCopy.drop('Grade', axis=1).values.tolist()[400:]
 #    
 #    
 #    return trainingSet, testSet, trainSetGrades, testSetGrades, trainSetFeatures, testSetFeatures
-  
-    
-def load_dataset(trainFileName, testFileName):
-    dataTrain = pd.read_csv(trainFileName, header=0)
-    trainSetGrades = dataTrain['Grade'].values.tolist()[:400]
-    
-    dataTrain['sex'] = dataTrain['sex'].map({'F': 0, 'M': 1})
-    dataTrain['address'] = dataTrain['address'].map({'U': 0, 'R': 1})
-    dataTrain['famsize'] = dataTrain['famsize'].map({'LE3': 0, 'GT3': 1})
-    dataTrain['Pstatus'] = dataTrain['Pstatus'].map({'T': 0, 'A': 1})
-    dataTrain['reason'] = dataTrain['reason'].map({'home': 0, 'reputation': 1, 'course' : 2, 'other' : 3})
-    dataTrain['guardian'] = dataTrain['guardian'].map({'mother': 0, 'father': 1, 'other' : 2})
-    dataTrain['schoolsup'] = dataTrain['schoolsup'].map({'yes': 0, 'no': 1})
-    dataTrain['famsup'] = dataTrain['famsup'].map({'yes': 0, 'no': 1})
-    dataTrain['paid'] = dataTrain['paid'].map({'yes': 0, 'no': 1})
-    dataTrain['activities'] = dataTrain['activities'].map({'yes': 0, 'no': 1})
-    dataTrain['higher'] = dataTrain['higher'].map({'yes': 0, 'no': 1})
-    dataTrain['internet'] = dataTrain['internet'].map({'yes': 0, 'no': 1})
-    dataTrain['romantic'] = dataTrain['romantic'].map({'yes': 0, 'no': 1})
-    
-    dataTrain  = dataTrain.astype(float)
-    
-    dataTrainCopy = deepcopy(dataTrain)
-    
-    trainingSet = dataTrain.values.tolist()[:400]
-    trainSetFeatures = dataTrain.drop('Grade', axis=1).values.tolist()[:400]
-        
-    
-    
-    #dataTest = pd.read_csv(testFileName, header=0)
-    testSetGrades = dataTrainCopy['Grade'].values.tolist()[400:]
-    
-    testSet = dataTrainCopy.values.tolist()[400:]
-    testSetFeatures = dataTrainCopy.drop('Grade', axis=1).values.tolist()[400:]
-    
-    
-    return trainingSet, testSet, trainSetGrades, testSetGrades, trainSetFeatures, testSetFeatures
                 
 '''
     Function that calculates Euclidian distance between any two given instances.
 '''
 def euclidean_distance(testInstance, trainingInstance, length):
     return math.sqrt(sum(pow(a-b,2) for a, b in zip(testInstance, trainingInstance)))
-#    distance = 0
-#    for x in range(length):
-#        distance += pow((testInstance[x] - trainingInstance[x]), 2)
-#    return math.sqrt(distance)
-
 
 
     
 def manhattan_distance(testInstance, trainingInstance, length):
     return sum(abs(a-b) for a,b in zip(testInstance,trainingInstance))
-#    distance = 0
-#    for x in range(length):
-#         distance += abs(testInstance[x] - trainingInstance[x])
-#    return distance
     
     
 '''
@@ -133,7 +144,7 @@ def get_neighbors(trainingSet, testInstance, k):
     distances = []
     length = len(testInstance)-1
     for x in range(len(trainingSet)):
-        #dist = chebyshev_distance(testInstance, trainingSet[x], length)
+        #dist = manhattan_distance(testInstance, trainingSet[x], length)
         dist = euclidean_distance(testInstance, trainingSet[x], length)
         distances.append((trainingSet[x], dist))
     distances.sort(key=operator.itemgetter(1))
@@ -181,6 +192,9 @@ def main():
 #    meanTestSet = np.mean(testSet)
 #    stdTestSet = np.std(testSet)
 #    
+#    meanTestGradeSet = np.mean(testSetGrades)
+#    stdTestGradeSet = np.std(testSetGrades)
+#    
 #    trainingSetNor = (trainingSet - meanTrainingSet) / stdTrainingSet
 #    testSetNor = (testSet - meanTestSet) / stdTestSet
 
@@ -199,16 +213,12 @@ def main():
 #        predictions.append(result)
 #        print('> predicted=' + repr(result) + ', actual=' + repr(testSetNor[x][-1]))
    
+    
     rmse = get_rmse(testSetGrades, predictions)
     print 'RMSE: ', rmse
     
     accuracy = get_accuracy(testSet, predictions)
     print('Accuracy: ' + repr(accuracy) + '%')
-    
-#    model = KNeighborsRegressor(n_neighbors=7, metric='euclidian')
-#    model.fit(trainSetFeatures, trainSetGrades)
-#    new_values = model.predict(testSetFeatures)
-#    print 'RMSE with sklearn: ' , get_rmse(testSetGrades, new_values)
     
     
 main()
