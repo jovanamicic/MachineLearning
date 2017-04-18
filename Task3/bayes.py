fc896f497b2e42c0f1c04930b0b641ed9ea3feef
Katarina <katarinapreradov@gmail.com>
2017-04-18 23:41:44 +0200

    podaljen train na 80-20 i dodato da se nepotpuna obelezja ne izbacuju vec mean kolone

--- Task3/bayes.py
+++ Task3/bayes.py
@@ -12,11 +12,18 @@ from sklearn.metrics import accuracy_score
 def load_dataset(filename_train, filename_test):
     #Train set
     dataTrain = pd.read_csv(filename_train, header=0)
+
     dataTrain = dataTrain.replace('?', np.nan)
+    dataTrain = dataTrain.astype(float)
+
+    #izbaci nedostajuca
     dataTrain = dataTrain.dropna()
-    dataTrain  = dataTrain.astype(float)
+
+    #? zameni sa mean te kolone
+    #dataTrain = dataTrain.apply(lambda x: x.fillna(x.mean()), axis=0)
+
     training_set = dataTrain.values.tolist()
-    
+
     training_set_mean_std = {0.0 : [], 1.0 : []}
 
     df_zeros = dataTrain.loc[dataTrain['contraceptive'] == 0]
@@ -25,21 +32,28 @@ def load_dataset(filename_train, filename_test):
     for column in df_zeros.columns.values:
         if column != 'contraceptive':
             training_set_mean_std[0.0].append(tuple((df_zeros[column].mean(), df_zeros[column].std())))
-        
+
     for column in df_ones.columns.values:
         if column != 'contraceptive':
             training_set_mean_std[1.0].append(tuple((df_ones[column].mean(), df_ones[column].std())))
-    
-    print training_set_mean_std
+
+    #print training_set_mean_std
 
     #Test set
     dataTest = pd.read_csv(filename_test, header=0)
+
     dataTest = dataTest.replace('?', np.nan)
+    dataTest = dataTest.astype(float)
+
+    #izbaci nedostajuce
     dataTest = dataTest.dropna()
-    dataTest  = dataTest.astype(float)
+
+    #zameni sa mean te kolone
+    #dataTest = dataTest.apply(lambda x: x.fillna(x.mean()), axis=0)
+
     actual_values = dataTest['contraceptive'].tolist()
     test_set = dataTest.values.tolist()
-    
+
     return training_set, test_set, actual_values, training_set_mean_std
  
 #def separateByClass(dataset):
@@ -70,27 +84,27 @@ def load_dataset(filename_train, filename_test):
 #    return summaries
 
 def calculateProbability(x, mean, stdev):
-	exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
-	return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
+    exponent = math.exp(-(math.pow(x-mean,2)/(2*math.pow(stdev,2))))
+    return (1 / (math.sqrt(2*math.pi) * stdev)) * exponent
 
 def calculateClassProbabilities(summaries, inputVector):
-	probabilities = {}
-	for classValue, classSummaries in summaries.iteritems():
-		probabilities[classValue] = 1
-		for i in range(len(classSummaries)):
-			mean, stdev = classSummaries[i]
-			x = inputVector[i]
-			probabilities[classValue] *= calculateProbability(x, mean, stdev)
-	return probabilities
+    probabilities = {}
+    for classValue, classSummaries in summaries.iteritems():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
 
 def predict(summaries, inputVector):
-	probabilities = calculateClassProbabilities(summaries, inputVector)
-	bestLabel, bestProb = None, -1
-	for classValue, probability in probabilities.iteritems():
-		if bestLabel is None or probability > bestProb:
-			bestProb = probability
-			bestLabel = classValue
-	return bestLabel
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.iteritems():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
 
  
 '''
@@ -100,9 +114,9 @@ def predict(summaries, inputVector):
  Ovo mozemo isto promeniti ako necemo da budu recnici.
 '''
 def getPredictions(summaries, testSet):
-	predictions = []
-	for i in range(len(testSet)):
-		result = predict(summaries, testSet[i])
-		predictions.append(result)
-	return predictions
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
 
--- Task3/test.py
+++ Task3/test.py
@@ -6,8 +6,8 @@ from bayes import *
 if __name__ == '__main__':
     # run: python test.py
     if len(sys.argv) == 1: 
-        filename_train = 'data/train.csv'
-        filename_test = 'data/test.csv'
+        filename_train = 'data/train2.csv'
+        filename_test = 'data/test2.csv'
        
     # run: python test.py filename_test
     elif len(sys.argv) == 2:
@@ -20,7 +20,8 @@ if __name__ == '__main__':
         filename_test = sys.argv[2]
 
     training_set, test_set, actual_values, training_set_mean_std = load_dataset(filename_train, filename_test)
-    
+
+
     predictions = getPredictions(training_set_mean_std, test_set)
     accuracy = accuracy_score(actual_values, predictions) * 100
     print('Accuracy: {0} %').format(accuracy)
