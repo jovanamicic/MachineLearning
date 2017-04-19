0bdf3201fbf22847ed50bf1152fbbb1dfb541613
MinaMedic <alekmina94@gmail.com>
2017-04-19 19:01:50 +0200

    Pocet kod po ugledu na pseudokod

--- Task3/multinomialNB_2.py
+++ Task3/multinomialNB_2.py
@@ -24,8 +24,8 @@ def load_dataset(filename_train, filename_test):
 
     training_dict = {0.0 : [], 1.0 : []}
 
-    df_zeros = dataTrain.loc[dataTrain['contraceptive'] == 0]
-    df_ones = dataTrain.loc[dataTrain['contraceptive'] == 1]
+    df_zeros = dataTrain.loc[dataTrain['isChina'] == 0]
+    df_ones = dataTrain.loc[dataTrain['isChina'] == 1]
 
     for row in  range(len(df_zeros.values.tolist())):
         training_dict[0.0].append(df_zeros.values.tolist()[row])
@@ -43,28 +43,81 @@ def load_dataset(filename_train, filename_test):
     #izbaci nedostajuce
     dataTest = dataTest.dropna()
 
-    actual_values = dataTest['contraceptive'].tolist()
+    actual_values = dataTest['isChina'].tolist()
     test_set = dataTest.values.tolist()
 
     return training_dict, test_set, actual_values
 
+#def fit(training_dict, alpha = 1.0):
+#
+#    #D is number of instances in training set
+#    D = len(training_dict[0.0]) + len(training_dict[1.0])
+#        
+#    class_log_prob = []
+#    for key in training_dict.keys():
+#        class_log_prob.append(np.log(1.0 * len(training_dict[key]) / D))
+#                                 
+#    #we need to count each values for each class and add self.alpha as smooting
+#    count = np.array([np.array(training_dict[key]).sum(axis=0) for key in training_dict]) + alpha
+#
+#    feature_log_prob = np.log(count / count.sum(axis=1)[np.newaxis].T)
+#
+#    return class_log_prob, feature_log_prob
+#
+#def predict(X, class_log_prob, feature_log_prob):
+#    pred = [(feature_log_prob * x).sum(axis = 1) + class_log_prob for x in X]
+#    return np.argmax(pred, axis = 1)
+
 def fit(training_dict, alpha = 1.0):
 
-    #D is number of instances in training set
-    D = len(training_dict[0.0]) + len(training_dict[1.0])
+    #ukupan broj instanci u dataset-u
+    N = float(len(training_dict[0.0]) + len(training_dict[1.0]))
+    
+    #broj instanci po klasi
+    Nc = [len(training_dict[key]) for key in training_dict]
+       
+    prior_c = [float(c/N) for c in Nc]
+       
+    #brojimo koliko se puta koji atribut (rec) pojavio u kojoj klasi
+    count = []
+    for key in training_dict:
+        count.append([sum(x) for x in zip(*training_dict[key])])
         
-    class_log_prob = []
-    for key in training_dict.keys():
-        class_log_prob.append(np.log(1.0 * len(training_dict[key]) / D))
-                                 
-    #we need to count each values for each class and add self.alpha as smooting
-    count = np.array([np.array(training_dict[key]).sum(axis=0) for key in training_dict]) + alpha
+    #dodajemo smooting parametar na svaku vrednsost
+    count_smoot = []
+    for i in count:
+        count_smoot.append(map(lambda x : x + alpha, i))
+    
+    #calculate the log probability of each word Tct
+    #suma frekvencija reci (atributa) d svih instanci koji pridapaju klasi c
+    
+    
 
-    feature_log_prob = np.log(count / count.sum(axis=1)[np.newaxis].T)
-
-    return class_log_prob, feature_log_prob
-
-def predict(X, class_log_prob, feature_log_prob):
-    pred = [(feature_log_prob * x).sum(axis = 1) + class_log_prob for x in X]
-    return np.argmax(pred, axis = 1)
-
+            
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
--- Task3/test_2.py
+++ Task3/test_2.py
@@ -8,8 +8,8 @@ from multinomialNB_2 import *
 if __name__ == '__main__':
     # run: python test.py
     if len(sys.argv) == 1: 
-        filename_train = 'data/train2.csv'
-        filename_test = 'data/test2.csv'
+        filename_train = 'drzave.csv'
+        filename_test = 'drzaveTest.csv'
        
     # run: python test.py filename_test
     elif len(sys.argv) == 2:
@@ -32,10 +32,10 @@ if __name__ == '__main__':
     training_dict, test_set, actual_values = load_dataset(filename_train, filename_test)
 
     class_log_prob, feature_log_prob = fit(training_dict)
-    predictions = predict(test_set, class_log_prob, feature_log_prob)
+    #predictions = predict(test_set, class_log_prob, feature_log_prob)
 
-    accuracy = accuracy_score(actual_values, predictions) * 100
-    print('Accuracy: {0} %').format(accuracy)
+    #accuracy = accuracy_score(actual_values, predictions) * 100
+    #print('Accuracy: {0} %').format(accuracy)
 
 # -*- coding: utf-8 -*-
 
