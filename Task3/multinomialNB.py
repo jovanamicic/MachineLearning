26d9c6c1cff8abe8a1a5ce3b31cb9a6fb615038e
Katarina <katarinapreradov@gmail.com>
2017-04-20 17:13:13 +0200

    description

--- Task3/multinomialNB.py
+++ Task3/multinomialNB.py
@@ -7,7 +7,8 @@ np.set_printoptions(precision=6)
 '''
     Function that load test and training set from CSV files.
     All columns with missing values are deleted.
-    Function also returns train data set with calculate mean and std for every attribute.
+    Function return training dataset separated by classes, and it returns
+    test features and test actual values.
 '''
 def load_dataset(filename_train, filename_test):
 
@@ -17,15 +18,24 @@ def load_dataset(filename_train, filename_test):
     dataTrain = dataTrain.replace('?', np.nan)
     dataTrain = dataTrain.astype(float)
 
-    #izbaci nedostajuca
+    # izbaci nedostajuca
     dataTrain = dataTrain.dropna()
 
-    #? zameni sa mean te kolone
-    #dataTrain = dataTrain.apply(lambda x: x.fillna(x.mean()), axis=0)
+    # zameni nedostajuce sa mean te kolone
+    # dataTrain = dataTrain.apply(lambda x: x.fillna(x.mean()), axis=0)
 
-    train_y = dataTrain['isChina'].tolist()
-    train_x = dataTrain.values.tolist()
+    training_dict = {0.0: [], 1.0: []}
 
+    df_zeros = dataTrain.loc[dataTrain['contraceptive'] == 0]
+    df_zeros = df_zeros.drop('contraceptive', 1)
+    df_ones = dataTrain.loc[dataTrain['contraceptive'] == 1]
+    df_ones = df_ones.drop('contraceptive', 1)
+
+    for row in  range(len(df_zeros.values.tolist())):
+        training_dict[0.0].append(df_zeros.values.tolist()[row])
+
+    for row in  range(len(df_ones.values.tolist())):
+        training_dict[1.0].append(df_ones.values.tolist()[row])
 
 
     # ------------------------ TEST SET ---------------------------
@@ -34,47 +44,60 @@ def load_dataset(filename_train, filename_test):
     dataTest = dataTest.replace('?', np.nan)
     dataTest = dataTest.astype(float)
 
-    #izbaci nedostajuce
+    # izbaci nedostajuce
     dataTest = dataTest.dropna()
 
-    #zameni sa mean te kolone
-    #dataTest = dataTest.apply(lambda x: x.fillna(x.mean()), axis=0)
+    # zameni nedostajuce sa mean te kolone
+    # dataTest = dataTest.apply(lambda x: x.fillna(x.mean()), axis=0)
 
-    actual_values = dataTest['isChina'].tolist()
-    test_set = dataTest.values.tolist()
+    actual_values = dataTest['contraceptive'].tolist()
+    test_set = dataTest.drop('contraceptive', 1).values.tolist()
+    return training_dict, test_set, actual_values
 
-    return train_x, train_y, test_set, actual_values
 
-    
-class MultinomialNB(object):
-    def __init__(self, alpha=1.0):
-        self.alpha = alpha  #smoothing param
+'''
+    Function that calculate probabilities based on data.
+'''
+def fit(training_dict, alpha=1.0):
 
-    def fit(self, X, y):
+    # total number of instances in training dataset
+    N = float(len(training_dict[0.0]) + len(training_dict[1.0]))
 
-        # group by class
-        separated = [[x for x, t in zip(X, y) if t == c] for c in np.unique(y)]
+    # total number of instances by classes
+    Nc = [len(training_dict[key]) for key in training_dict]
 
-        #calculate prior log probability for each class
-        count_sample = X.shape[0]
+    # probability for each class
+    class_probabilities = [(1.0 * c / N) for c in Nc]
 
-        self.class_log_prior_ = [(1.0 * len(i) / count_sample) for i in separated]
-        print self.class_log_prior_
+    # counting appearance of each attribute in class
+    count = []
+    for key in training_dict:
+        count.append([sum(x) for x in zip(*training_dict[key])])
 
-        count = np.array([np.array(i).sum(axis=0) for i in separated]) + self.alpha
-        print
-        print count.tolist()
-        print
+    # adding smoothing parameter on each value
+    count_smooth = []
+    for i in count:
+        count_smooth.append(map(lambda x: x + alpha, i))
 
-        self.feature_log_prob_ = (count / count.sum(axis=1)[np.newaxis].T)
-        print self.feature_log_prob_
+    # sum of frequencies of all words in all instances which belong to class c
+    feature_probabilities = []
+    for c in count_smooth:
+        feature_probabilities.append((np.array(c) / sum(c)).tolist())
 
-        return self
+    return class_probabilities, feature_probabilities
 
-    def predict_log_proba(self, X):
-        return [(self.feature_log_prob_ * x).sum(axis=1) + self.class_log_prior_
-                for x in X]
 
-    def predict(self, X):
-        return np.argmax(self.predict_log_proba(X), axis=1)
+'''
+    Function that makes predictions for test data.
+'''
+def predict(test_set, class_probabilities, feature_probabilities):
 
+    # making numpy arrays
+    feature_probabilities = np.log(np.array(feature_probabilities))
+    class_probabilities = np.log(np.array(class_probabilities))
+
+    prediction = []
+    for x in test_set:
+        prediction.append((np.array(feature_probabilities) * np.array(x)).sum(axis=1) + class_probabilities)
+
+    return np.argmax(prediction, axis=1)  # returns index of max element
--- Task3/test.py
+++ Task3/test.py
@@ -1,15 +1,14 @@
 import sys
-import numpy as np
 from sklearn.metrics import accuracy_score
 
-from multinomialNB import *
+from multinomialNB import load_dataset, fit, predict
 
 
 if __name__ == '__main__':
     # run: python test.py
     if len(sys.argv) == 1: 
-        filename_train = 'drzave.csv'
-        filename_test = 'drzaveTest.csv'
+        filename_train = 'data/train.csv'
+        filename_test = 'data/test.csv'
        
     # run: python test.py filename_test
     elif len(sys.argv) == 2:
@@ -21,23 +20,11 @@ if __name__ == '__main__':
         filename_train = sys.argv[1]
         filename_test = sys.argv[2]
 
-    # read dataset for gaussian NB
-    # training_set, test_set, actual_values, training_set_mean_std = load_dataset(filename_train, filename_test)
+    training_dict, test_set, actual_values = load_dataset(filename_train, filename_test)
 
-    # predictions for gaussian NB
-    #predictions = getPredictions(training_set_mean_std, test_set)
-
-
-    # read dataset for multinomial/bernouli NB
-    train_x, train_y, test_set, actual_values = load_dataset(filename_train, filename_test)
-
-    model = MultinomialNB()
-    model.fit(np.array(train_x), np.array(train_y))
-    predictions = model.predict(test_set)
-
+    class_probabilities, feature_probabilities = fit(training_dict)
+    predictions = predict(test_set, class_probabilities, feature_probabilities)
 
     accuracy = accuracy_score(actual_values, predictions) * 100
     print('Accuracy: {0} %').format(accuracy)
 
-    #sa nasim: Accuracy: 99.5575221239 %
-    #sa ugradjenim: Accuracy: 99.5575221239 %
